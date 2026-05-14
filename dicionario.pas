5c4f4c0705d472d7755a0e4a7c5835ef2b969edc
lessamarcos <marcos.lessa@unidavi.edu.br>
2026-05-14 09:22:01 -0300

    procedimento de inserção de palavra

--- dicionario.pas
+++ dicionario.pas
@@ -20,3 +20,54 @@ Tlista = record
     fim: Tponteiro;
 end;
 
+function verificaVazio(l: tlista): boolean;
+begin
+    verificaVazio:= l.inicio = nil;
+end;
+
+procedure inserirPalavra(var l: Tlista; p: string)
+var novo, temp, aux: Tponteiro;
+begin
+    new(novo);
+    novo^.prox:= nil;
+    novo^.ant:= nil;
+    if novo <> nil then
+    begin
+        novo^.palavra:= p;
+        if verificaVazio(l) then
+        begin
+            l.inicio:= novo;
+            l.fim:= novo
+        end
+        else if p < l.inicio^.dado then
+        begin
+            temp:= l.inicio;
+            l.inicio:= novo;
+            novo^.prox:= temp;
+            temp^.ant:= novo;
+        end
+        else
+        begin
+            aux:= l.inicio;
+            while (aux^.prox <> nil) and (aux^.prox^.palavra < p) do 
+                aux:= aux^.prox;
+            if aux^.prox = nil then
+            begin
+                aux^.prox:= novo;
+                novo^.ant:= aux
+                l.fim:= novo;
+            end
+            else
+            begin
+                temp:= aux^.prox;
+                aux^.prox:= novo;
+                novo^.prox:= temp;
+                novo^.ant:= aux;
+                temp^.ant:= novo;
+            end;
+        end;
+    end
+    else
+        writeln('Memória cheia hahaha');
+end;
+
